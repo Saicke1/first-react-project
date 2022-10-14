927d38d549ecf598e70d4f963339e0403a37c6e6
Sarah Manicke <sm.wit2022@startsteps.de>
2022-10-14 15:23:50 +0200

    creating chat and make it work

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import "./Chat.css";
+import { authContext } from "../useContext/UserAuthContext";
+import Button from "react-bootstrap/Button";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 const Chat = () => {
+  const { user } = useContext(authContext);
+  console.log("user", user);
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const getMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  //function for sending messages or adding them into the firestore and the app itself
+  const sendMessage = async () => {
+    const docRef = await addDoc(collection(db, "chatMessages"), {
+      name: user.displayName,
+      text: message,
+      time: new Date(),
+    });
+    console.log("Document and message have the id:", docRef.id);
+    getWholeChat();
+  };
+
+  //function for getting the messages from firestore
+  const getWholeChat = async () => {
+    /* console.log("fetched messages"); */
+    const allMessages = await getDocs(collection(db, "chatMessages"));
+    const array = [];
+    allMessages.forEach((each) => {
+      // each.data() is never undefined for allMessages
+      /* console.log("each", each);
+      console.log(each.id, " => ", each.data()); */
+      array.push(each.data());
+    });
+    const sortedArray = array.sort(
+      (objA, objB) => Number(objA.time) - Number(objB.time)
+    );
+    setChatMessages(sortedArray);
+    /* console.log("Array in chatMessages", chatMessages); */
+  };
+
+  useEffect(() => {
+    getWholeChat();
+  }, []);
+
   return (
-    <div>
+    <div className="chatContainer">
       <h1>Das ist der Chat! perfekt, du bist eingeloggt. :D</h1>
+
+      {chatMessages &&
+        chatMessages.map((msg, index) => (
+          <div key={index} className="oneMessageBox">
+            <p>{msg.name}</p>
+            <p>{msg.text}</p>
+            <p>{msg.time.toDate().toLocaleString("de")}</p>
+          </div>
+        ))}
+
+      <input type="text" className="inputField" onChange={getMessage}></input>
+      <Button variant="primary" onClick={sendMessage}>
+        Send
+      </Button>
     </div>
   );
 };
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,7 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-/* import { getAdditionalUserInfo } from "firebase/auth"; */
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,6 +24,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
 export default app;
 export const auth = getAuth(app);
-/* export const addInfo = getAdditionalUserInfo(app); */
