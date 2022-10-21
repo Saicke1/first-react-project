f090978481fbbcbddedf0b17909f671b2c62649d
Sarah Manicke <sm.wit2022@startsteps.de>
2022-10-21 10:00:16 +0200

    insert Sound

--- src/components/deletedUser/DeletedUser.js
+++ src/components/deletedUser/DeletedUser.js
@@ -3,18 +3,21 @@ import "./DeletedUser.css";
 import myGif from "../../shocked.gif";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import audio from "./killedKenny.mp3";
 
 const DeletedUser = () => {
   return (
     <div className="deletedContainer">
       <h1>You are deleted!</h1>
       <h5>"OMG! You killed Kenny!"</h5>
+      <h5>"You Bastards!"</h5>
       <img src={myGif} alt="shocked" className="gifSize" />
       <Link to="/home">
         <Button style={{ backgroundColor: "#0FA4EE" }}>
           Go back to Home Page
         </Button>
       </Link>
+      <audio autoPlay src={audio}></audio>
     </div>
   );
 };
