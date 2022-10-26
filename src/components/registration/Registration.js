36525df70f2e674cbf6681e42426db5ab2af5a69
Sarah Manicke <sm.wit2022@startsteps.de>
2022-10-26 09:43:32 +0200

    improved the registration page

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -2,26 +2,38 @@ import React, { useContext, useState } from "react";
 import "./Registration.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { authContext } from "../useContext/UserAuthContext";
+import Alert from "react-bootstrap/Alert";
 
 const Registration = () => {
   const { registerUser } = useContext(authContext);
-
+  const navigate = useNavigate();
   const [registerMail, setRegisterMail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
+  const [error, setError] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
   }
 
-  const register = () => {
-    registerUser(registerMail, registerPassword);
+  const register = async (e) => {
+    e.preventDefault();
+    if (registerPassword.length > 5) {
+      await registerUser(registerMail, registerPassword);
+      navigate("/profile");
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <div className="registrationContainer">
       <h1>Become a new douchebag!</h1>
+
+      {error && (
+        <Alert variant="danger">Your password isn't long enough!</Alert>
+      )}
 
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -38,6 +50,9 @@ const Registration = () => {
         <Form.Group className="mb-2" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
+          <Form.Text className="text-muted">
+            Password needs at least 6 characters!
+          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="passwordRepeat">
@@ -49,18 +64,17 @@ const Registration = () => {
             }}
           />
           <Form.Text className="text-muted">
-            Don't look at me like that, it's needed!
+            "Don't look at me like that, it's needed!"
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check, so we can spam you!" />
         </Form.Group>
-        <Link to="/profile">
-          <Button variant="primary" type="submit" onClick={register}>
-            Create Account
-          </Button>
-        </Link>
+
+        <Button variant="primary" type="submit" onClick={register}>
+          Create Account
+        </Button>
       </Form>
     </div>
   );
